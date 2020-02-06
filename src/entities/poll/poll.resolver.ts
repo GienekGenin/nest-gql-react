26ff0f5415c84b053c7aca7abe1a7bbde7af0935
GienekGenin <genin.gennadiigendos2010@yandex.ru>
2020-02-06 16:28:02 +0100

    Custom decorator

--- src/entities/poll/poll.resolver.ts
+++ src/entities/poll/poll.resolver.ts
@@ -1,12 +1,20 @@
-import { Resolver, Mutation } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/common/guards/auth.guard';
+import { GetUserId } from 'src/common/decorators/getUserId.decorator';
+import { CreatePollArgs } from '../../common/args/createPoll.args';
+import { PollService } from './poll.service';
 
 @Resolver('Poll')
 export class PollResolver {
+  constructor(private readonly pollService: PollService) {}
+
   @Mutation(() => Boolean)
   @UseGuards(AuthGuard)
-  async createPoll() {
-    return true;
+  async createPoll(
+    @GetUserId() userId: string,
+    @Args() { name, options }: CreatePollArgs,
+  ): Promise<boolean> {
+    return this.pollService.createPoll(userId, name, options);
   }
 }
